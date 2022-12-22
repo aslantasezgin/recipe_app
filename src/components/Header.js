e38ced25661a016bd52b4766f0aeae51e3c05b10
John Doe <aslantasezginn@gmail.com>
2022-12-22 21:12:41 +0300

    tailwind added and created a api_url

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+
+const Header = () => {
+
+    const logo = "https://cdn.dribbble.com/users/131733/screenshots/5472304/artboard_copy.png";
+
+    return(
+        <nav>
+
+        </nav>
+    )
+}
+
+export default Header
---
+++ src/const/index.js
@@ -0,0 +1,4 @@
+
+export const API_URL = "https://tasty.p.rapidapi.com/recipes/list"
+export const API_TOKEN = "b81fc2791bmshb58385331c382f1p1f6512jsn01b9a22e300a"
+export const API_HOST = "tasty.p.rapidapi.com"
